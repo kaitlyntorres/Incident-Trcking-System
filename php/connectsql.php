b1cce3fcaf40545f2ec609a85c37edb2ffb14671
Kaitlyn Torres <71412255+kaitlyntorres@users.noreply.github.com>
2021-11-19 12:12:31 -0500

    Update connectsql.php

--- php/connectsql.php
+++ php/connectsql.php
@@ -1,1 +1,13 @@
-$conn= new mysqli($servername, $username, $password);
+<?php>
+$username=username
+$password= "Sick1234"
+// name of database
+$db="users"
+$conn= mysqli_connect("localhost",$username, $password, $db);
+
+// referenced from w3schools.com
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} 
+
+?>
